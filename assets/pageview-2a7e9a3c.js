b15eece5db08d5e0e025f5536a3ccba899baad8f
EvanSnowly <2972561826@qq.com>
2023-11-03 19:13:23 +0800

    错题组件

---
+++ assets/pageview-2a7e9a3c.js
@@ -0,0 +1,1 @@
+import{K as r}from"./app-4ce8496b.js";const o=async()=>{try{const{pageviewCount:e}=await r(()=>import("./pageview_vuepress-plugin-comment2-b25bb000.js"),[]);return e({serverURL:COMMENT_OPTIONS.serverURL})}catch{console.error("@waline/client is not installed!");return}};export{o as updatePageview};
